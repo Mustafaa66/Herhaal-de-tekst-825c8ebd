667f9e3b32acc1e05620739a7d3578384d23a0b8
Mustafa Cansever <45204423+Mustafaa66@users.noreply.github.com>
2020-04-07 17:43:56 +0200

    Add files via upload

---
+++ herhaalTekst.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Herhaal de tekst</title>
+</head>
+
+<body>
+
+<?php
+for ($x = 0; $x <= 4; $x++) {
+  echo "Ik ben Mustafa <br>";
+}
+?>
+
+
+
+</body>
+
+</html>
